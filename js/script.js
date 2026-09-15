6faa13635232be21b7ea012f7b78d6aae605e470
frost <frostfire732@gmail.com>
2026-09-15 18:29:20 +0700

    Sea Diver

--- js/script.js
+++ js/script.js
@@ -12,22 +12,6 @@
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext("2d");
 
-  // Polyfill cho ctx.roundRect nếu trình duyệt cũ chưa hỗ trợ
-  if (!CanvasRenderingContext2D.prototype.roundRect) {
-    CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, radii) {
-      if (!Array.isArray(radii)) radii = [radii || 0];
-      const r = Math.min(radii[0] || 0, w / 2, h / 2);
-      this.beginPath();
-      this.moveTo(x + r, y);
-      this.arcTo(x + w, y, x + w, y + h, r);
-      this.arcTo(x + w, y + h, x, y + h, r);
-      this.arcTo(x, y + h, x, y, r);
-      this.arcTo(x, y, x + w, y, r);
-      this.closePath();
-      return this;
-    };
-  }
-
   const starCountEl = document.getElementById("starCount");
   const depthCountEl = document.getElementById("depthCount");
   const btnSound = document.getElementById("btnSound");
